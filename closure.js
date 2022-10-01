d589d3b6d8a2fb5e39f82d2285bace54b4c48176
Danishk69 <114822485+Danishk69@users.noreply.github.com>
2022-10-01 21:25:37 +0530

    Update closure.js

--- closure.js
+++ closure.js
@@ -12,6 +12,9 @@ var sum = function (a,b,c,d,e) {
         getSumFive:function(){
             return a+b+c+d+e;
         }
+        getSumSix:function(){
+            return a+b+c+d+e;
+        }
     }
 }
 
@@ -20,3 +23,4 @@ console.log(value.getSumTwo());
 console.log(value.getSumThree());
 console.log(value.getSumFour());
 console.log(value.getSumFive());
+console.log(value.getSumSix());
